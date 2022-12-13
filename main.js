88b4c61ae432cea86f2007698bc35b01964e99f5
Hassan Qudus <114084917+Qudus2005@users.noreply.github.com>
2022-12-13 13:53:43 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+// alert('This Webpage Is Dedicated To All Web Designers In The World')
+
+function red(){
+    var red = document.getElementById('about-box').style.backgroundColor = 'red'
+}
+// function green(){
+//     var green = document.getElementById('about-box').style.backgroundColor = 'black'
+// }
+    function blue(){
+        var blue = document.getElementById('about-box').style.backgroundColor = 'blue'
+    }
+    
+    function brown(){
+        var brown = document.getElementById('about-box').style.backgroundColor = 'brown'
+    }
